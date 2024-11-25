32bc5b4758aeefc79514ecfdea825417cf432faf
Pierre-Olivier Tremblay <pierre-olivier.tremblay.8@ulaval.ca>
2024-11-25 17:48:52 -0500

    modification questions/fix Bonjour X!

--- interface.pl
+++ interface.pl
@@ -2,20 +2,13 @@
 :- consult('knowledge_base.pl').
 :- consult('utility.pl').
 
+:- dynamic available_days/1. % Permet de déclarer un jour de la semaine comme étant disponible
+:- dynamic available_hours/1. % Permet de déclarer une plage horaire comme étant disponible
+:- dynamic available_genres/1. % Permet de déclarer un genre de film comme étant disponible
+
 cls :- 
     write('\33\[2J'),
     write('\e[2J').
-
-:- dynamic available_days/1. % Permet de déclarer un jour de la semaine comme étant disponible
-
-% Jour de la semaine
-weekday(1, 'Lundi').
-weekday(2, 'Mardi').
-weekday(3, 'Mercredi').
-weekday(4, 'Jeudi').
-weekday(5, 'Vendredi').
-weekday(6, 'Samedi').
-weekday(7, 'Dimanche').
 
 handle_weekdays_question :-
     write('Quelles jours de la semaine êtes-vous disponible?'), nl, nl,
@@ -44,9 +37,81 @@ handle_weekdays_question :-
         )
     ).
 
+handle_schedule_question :-
+    write('Quelle(s) plage(s) horaire(s) vous convient?'), nl, nl,
+    write_schedule(1, 'AM'),
+    write_schedule(2, 'PM'),
+    write_schedule(3, 'Soir'),
+    typewriter_write('0. Question suivante'), nl,
+    typewriter_write('Veuillez choisir un numéro correspondant à une plage horaire:'), nl,
+    read(Hours),
+    cls,
+    (Hours < 0 ; Hours > 3 -> 
+        true
+    ;
+        (Hours \= 0 ->
+            (available_hours(Hours) -> 
+                retract(available_hours(Hours))
+            ;   
+                assert(available_hours(Hours))),
+            handle_schedule_question
+        ; 
+            true
+        )
+    ).
+
+handle_genre_question :-
+    write('Quel(s) genre(s) de film préférez-vous aller voir?'), nl, nl,
+    write_genre(1, 'Action'),
+    write_genre(2, 'Drame'),
+    write_genre(3, 'Crime'),
+    write_genre(4, 'Western'),
+    write_genre(5, 'Horreur'),
+    write_genre(6, 'Animation'),
+    write_genre(7, 'Biographie'),
+    write_genre(8, 'Comédie'),
+    write_genre(9, 'Musical'),
+    write_genre(10, 'Thriller'),
+    write_genre(11, 'Sci-Fi'),
+    write_genre(12, 'Famille'),
+    write_genre(13, 'Fantaisie'),
+    write_genre(14, 'Romance'),
+    write_genre(15, 'Aventure'),
+    typewriter_write('0. Question suivante'), nl,
+    typewriter_write('Veuillez choisir un numéro correspondant à un genre de film:'), nl,
+    read(Genre),
+    cls,
+    (Genre < 0 ; Genre > 15 -> 
+        true
+    ;   
+        (Genre \= 0 ->
+            (available_genres(Genre) -> 
+                retract(available_genres(Genre))
+            ;   
+                assert(available_genres(Genre))),
+            handle_genre_question
+        ; 
+            true
+        )
+    ).
+
+handle_budget_question :-
+    write('Quel est votre budget pour la sortie au cinema?'), nl,
+    write('Veuillez entrer un montant en dollars:'), nl,
+    read(Budget),
+    cls.
+
 write_day(Number, DayName) :-
     typewriter_write(Number), typewriter_write('. '), typewriter_write(DayName),
     (available_days(Number) -> typewriter_write(' \033[32m(Déjà sélectionné)\033[0m') ; true), nl.
+
+write_schedule(Number, ScheduleName) :-
+    typewriter_write(Number), typewriter_write('. '), typewriter_write(ScheduleName),
+    (available_hours(Number) -> typewriter_write(' \033[32m(Déjà sélectionné)\033[0m') ; true), nl.
+
+write_genre(Number, GenreName) :-    
+    typewriter_write(Number), typewriter_write('. '), typewriter_write(GenreName),
+    (available_genres(Number) -> typewriter_write(' \033[32m(Déjà sélectionné)\033[0m') ; true), nl.
 
 %Interface principal
 start_ui :-
@@ -59,19 +124,16 @@ start_ui :-
 
     cls,
     typewriter_write('\n Quel est votre nom?'), nl,
-    read(Name),
+    read_line_to_string(user_input, Name),
 
     cls,
-    typewriter_write('Bonjour '), typewriter_write(Name), typewriter_write('!'), nl,
-    nl, nl,
+    typewriter_write('Bonjour '), typewriter_write(Name), nl,
+    nl, write('(Appuyez sur une touche pour continuer...)'), nl,
 
-    handle_weekdays_question.
-
-    % write('Quelles sont les heures qui vous conviennent?'), nl,
-    % read(_), % a implementer ajouter la loop pour les heures selon les journees disponibles
-
-    % write('Quel genre de film préférez-vous aller voir?'), nl,
-    % read(_), % a implementer ajouter la loop pour les genres
+    handle_weekdays_question,
+    handle_schedule_question,
+    handle_genre_question.
+    
 
     % write('Quel est votre budget pour la sortie au cinema?'), nl,
     % read(_),
--- knowledge_base.pl
+++ knowledge_base.pl
@@ -1,90 +1,90 @@
-% Syntax: movie(Title, LeadActor, Rating, Location, Genre, RatingCategory).
-movie('The Shawshank Redemption', 'Tim Robbins', 9.3, 'Ste-Foy', 'Drama', 'R').
-movie('The Godfather', 'Marlon Brando', 9.2, 'Beauport', 'Crime', 'R').
-movie('The Dark Knight', 'Christian Bale', 9.0, 'Limoilou', 'Action', 'PG-13').
-movie('The Godfather Part II', 'Al Pacino', 9.0, 'Ste-Foy', 'Crime', 'R').
-movie('12 Angry Men', 'Henry Fonda', 9.0, 'Beauport', 'Drama', 'PG').
-movie('Schindler\'s List', 'Liam Neeson', 8.9, 'Limoilou', 'Biography', 'R').
-movie('The Lord of the Rings: The Return of the King', 'Elijah Wood', 8.9, 'Ste-Foy', 'Fantasy', 'PG-13').
-movie('Pulp Fiction', 'John Travolta', 8.9, 'Beauport', 'Crime', 'R').
-movie('The Lord of the Rings: The Fellowship of the Ring', 'Elijah Wood', 8.8, 'Limoilou', 'Fantasy', 'PG-13').
-movie('Forrest Gump', 'Tom Hanks', 8.8, 'Ste-Foy', 'Drama', 'PG-13').
-movie('Inception', 'Leonardo DiCaprio', 8.8, 'Beauport', 'Sci-Fi', 'PG-13').
-movie('The Lord of the Rings: The Two Towers', 'Elijah Wood', 8.8, 'Limoilou', 'Fantasy', 'PG-13').
-movie('Fight Club', 'Brad Pitt', 8.8, 'Ste-Foy', 'Drama', 'R').
-movie('The Empire Strikes Back', 'Mark Hamill', 8.7, 'Beauport', 'Sci-Fi', 'PG').
-movie('The Matrix', 'Keanu Reeves', 8.7, 'Limoilou', 'Sci-Fi', 'R').
-movie('Goodfellas', 'Robert De Niro', 8.7, 'Ste-Foy', 'Crime', 'R').
-movie('One Flew Over the Cuckoo\'s Nest', 'Jack Nicholson', 8.7, 'Beauport', 'Drama', 'R').
-movie('Se7en', 'Brad Pitt', 8.6, 'Limoilou', 'Crime', 'R').
-movie('The Silence of the Lambs', 'Jodie Foster', 8.6, 'Ste-Foy', 'Thriller', 'R').
-movie('It\'s a Wonderful Life', 'James Stewart', 8.6, 'Beauport', 'Drama', 'PG').
-movie('Parasite', 'Song Kang-ho', 8.6, 'Limoilou', 'Thriller', 'R').
-movie('Saving Private Ryan', 'Tom Hanks', 8.6, 'Ste-Foy', 'War', 'R').
-movie('The Green Mile', 'Tom Hanks', 8.6, 'Beauport', 'Drama', 'R').
-movie('Interstellar', 'Matthew McConaughey', 8.6, 'Limoilou', 'Sci-Fi', 'PG-13').
-movie('City of God', 'Alexandre Rodrigues', 8.6, 'Ste-Foy', 'Crime', 'R').
-movie('Life Is Beautiful', 'Roberto Benigni', 8.6, 'Beauport', 'Comedy', 'PG-13').
-movie('Spirited Away', 'Rumi Hiiragi', 8.6, 'Limoilou', 'Animation', 'PG').
-movie('The Usual Suspects', 'Kevin Spacey', 8.5, 'Ste-Foy', 'Crime', 'R').
-movie('Léon: The Professional', 'Jean Reno', 8.5, 'Beauport', 'Crime', 'R').
-movie('The Lion King', 'Matthew Broderick', 8.5, 'Limoilou', 'Animation', 'G').
-movie('The Pianist', 'Adrien Brody', 8.5, 'Ste-Foy', 'Biography', 'R').
-movie('Terminator 2: Judgment Day', 'Arnold Schwarzenegger', 8.5, 'Beauport', 'Action', 'R').
-movie('Gladiator', 'Russell Crowe', 8.5, 'Limoilou', 'Action', 'R').
-movie('American History X', 'Edward Norton', 8.5, 'Ste-Foy', 'Drama', 'R').
-movie('The Departed', 'Leonardo DiCaprio', 8.5, 'Beauport', 'Crime', 'R').
-movie('Whiplash', 'Miles Teller', 8.5, 'Limoilou', 'Drama', 'R').
-movie('The Prestige', 'Hugh Jackman', 8.5, 'Ste-Foy', 'Drama', 'PG-13').
-movie('The Dark Knight Rises', 'Christian Bale', 8.4, 'Beauport', 'Action', 'PG-13').
-movie('Avengers: Endgame', 'Robert Downey Jr.', 8.4, 'Limoilou', 'Action', 'PG-13').
-movie('Django Unchained', 'Jamie Foxx', 8.4, 'Ste-Foy', 'Western', 'R').
-movie('The Shining', 'Jack Nicholson', 8.4, 'Beauport', 'Horror', 'R').
-movie('WALL·E', 'Ben Burtt', 8.4, 'Limoilou', 'Animation', 'G').
-movie('Coco', 'Anthony Gonzalez', 8.4, 'Ste-Foy', 'Animation', 'PG').
-movie('Braveheart', 'Mel Gibson', 8.4, 'Beauport', 'Biography', 'R').
-movie('Joker', 'Joaquin Phoenix', 8.4, 'Limoilou', 'Drama', 'R').
-movie('Avengers: Infinity War', 'Robert Downey Jr.', 8.4, 'Ste-Foy', 'Action', 'PG-13').
-movie('Toy Story', 'Tom Hanks', 8.3, 'Beauport', 'Animation', 'G').
-movie('The Wolf of Wall Street', 'Leonardo DiCaprio', 8.2, 'Limoilou', 'Biography', 'R').
-movie('The Social Network', 'Jesse Eisenberg', 7.8, 'Ste-Foy', 'Biography', 'PG-13').
-movie('Cats', 'James Corden', 2.7, 'Limoilou', 'Musical', 'PG').
-movie('The Room', 'Tommy Wiseau', 3.7, 'Ste-Foy', 'Drama', 'R').
-movie('Disaster Movie', 'Matt Lanter', 1.9, 'Beauport', 'Comedy', 'PG-13').
-movie('Birdemic: Shock and Terror', 'Alan Bagh', 1.8, 'Ste-Foy', 'Horror', 'R').
-movie('Battlefield Earth', 'John Travolta', 2.5, 'Limoilou', 'Sci-Fi', 'PG-13').
-movie('Manos: The Hands of Fate', 'Tom Neyman', 1.6, 'Beauport', 'Horror', 'PG').
-movie('Movie 43', 'Hugh Jackman', 4.3, 'Ste-Foy', 'Comedy', 'R').
-movie('Dragonball Evolution', 'Justin Chatwin', 2.6, 'Limoilou', 'Action', 'PG').
-movie('Superbabies: Baby Geniuses 2', 'Jon Voight', 1.9, 'Beauport', 'Comedy', 'G').
-movie('Gigli', 'Ben Affleck', 2.6, 'Ste-Foy', 'Romance', 'R').
-movie('The Love Guru', 'Mike Myers', 3.8, 'Limoilou', 'Comedy', 'PG-13').
-movie('House of the Dead', 'Jonathan Cherry', 2.1, 'Beauport', 'Horror', 'R').
-movie('Alone in the Dark', 'Christian Slater', 2.4, 'Ste-Foy', 'Horror', 'R').
-movie('Son of the Mask', 'Jamie Kennedy', 2.2, 'Limoilou', 'Comedy', 'PG').
-movie('Kirk Cameron\'s Saving Christmas', 'Kirk Cameron', 1.3, 'Beauport', 'Family', 'G').
-movie('Epic Movie', 'Kal Penn', 2.4, 'Ste-Foy', 'Comedy', 'PG-13').
-movie('The Wicker Man', 'Nicolas Cage', 3.7, 'Limoilou', 'Thriller', 'PG-13').
-movie('Battle: Los Angeles', 'Aaron Eckhart', 5.7, 'Beauport', 'Action', 'PG-13').
-movie('Pixels', 'Adam Sandler', 5.6, 'Ste-Foy', 'Comedy', 'PG-13').
-movie('Transformers: Revenge of the Fallen', 'Shia LaBeouf', 6.0, 'Limoilou', 'Action', 'PG-13').
-movie('The Happening', 'Mark Wahlberg', 5.0, 'Beauport', 'Thriller', 'R').
-movie('Fantastic Four', 'Miles Teller', 4.3, 'Ste-Foy', 'Sci-Fi', 'PG-13').
-movie('Ghost Rider: Spirit of Vengeance', 'Nicolas Cage', 4.3, 'Limoilou', 'Action', 'PG-13').
-movie('Suicide Squad', 'Will Smith', 5.9, 'Beauport', 'Action', 'PG-13').
-movie('The Twilight Saga: New Moon', 'Kristen Stewart', 4.7, 'Ste-Foy', 'Fantasy', 'PG-13').
-movie('Batman & Robin', 'George Clooney', 3.7, 'Limoilou', 'Action', 'PG-13').
-movie('The Last Airbender', 'Noah Ringer', 4.1, 'Beauport', 'Adventure', 'PG').
-movie('Jaws: The Revenge', 'Michael Caine', 3.0, 'Ste-Foy', 'Thriller', 'PG-13').
-movie('50 Shades of Grey', 'Dakota Johnson', 4.1, 'Limoilou', 'Romance', 'R').
-movie('The Emoji Movie', 'T.J. Miller', 3.3, 'Beauport', 'Animation', 'PG').
+% Syntax: movie(Title, LeadActor, Rating, Location, Genre, RatingCategory, TimeOfDay).
+movie('The Shawshank Redemption', 'Tim Robbins', 9.3, 'Ste-Foy', 'Drame', 'R', 'PM').
+movie('The Godfather', 'Marlon Brando', 9.2, 'Beauport', 'Crime', 'R', 'Soir').
+movie('The Dark Knight', 'Christian Bale', 9.0, 'Limoilou', 'Action', 'PG-13', 'AM').
+movie('The Godfather Part II', 'Al Pacino', 9.0, 'Ste-Foy', 'Crime', 'R', 'PM').
+movie('12 Angry Men', 'Henry Fonda', 9.0, 'Beauport', 'Drame', 'PG', 'Soir').
+movie('Schindler\'s List', 'Liam Neeson', 8.9, 'Limoilou', 'Biographie', 'R', 'AM').
+movie('The Lord of the Rings: The Return of the King', 'Elijah Wood', 8.9, 'Ste-Foy', 'Fantaisie', 'PG-13', 'PM').
+movie('Pulp Fiction', 'John Travolta', 8.9, 'Beauport', 'Crime', 'R', 'Soir').
+movie('The Lord of the Rings: The Fellowship of the Ring', 'Elijah Wood', 8.8, 'Limoilou', 'Fantaisie', 'PG-13', 'AM').
+movie('Forrest Gump', 'Tom Hanks', 8.8, 'Ste-Foy', 'Drame', 'PG-13', 'PM').
+movie('Inception', 'Leonardo DiCaprio', 8.8, 'Beauport', 'Sci-Fi', 'PG-13', 'Soir').
+movie('The Lord of the Rings: The Two Towers', 'Elijah Wood', 8.8, 'Limoilou', 'Fantaisie', 'PG-13', 'AM').
+movie('Fight Club', 'Brad Pitt', 8.8, 'Ste-Foy', 'Drame', 'R', 'PM').
+movie('The Empire Strikes Back', 'Mark Hamill', 8.7, 'Beauport', 'Sci-Fi', 'PG', 'Soir').
+movie('The Matrix', 'Keanu Reeves', 8.7, 'Limoilou', 'Sci-Fi', 'R', 'AM').
+movie('Goodfellas', 'Robert De Niro', 8.7, 'Ste-Foy', 'Crime', 'R', 'PM').
+movie('One Flew Over the Cuckoo\'s Nest', 'Jack Nicholson', 8.7, 'Beauport', 'Drame', 'R', 'Soir').
+movie('Se7en', 'Brad Pitt', 8.6, 'Limoilou', 'Crime', 'R', 'AM').
+movie('The Silence of the Lambs', 'Jodie Foster', 8.6, 'Ste-Foy', 'Thriller', 'R', 'PM').
+movie('It\'s a Wonderful Life', 'James Stewart', 8.6, 'Beauport', 'Drame', 'PG', 'Soir').
+movie('Parasite', 'Song Kang-ho', 8.6, 'Limoilou', 'Thriller', 'R', 'AM').
+movie('Saving Private Ryan', 'Tom Hanks', 8.6, 'Ste-Foy', 'Guerre', 'R', 'PM').
+movie('The Green Mile', 'Tom Hanks', 8.6, 'Beauport', 'Drame', 'R', 'Soir').
+movie('Interstellar', 'Matthew McConaughey', 8.6, 'Limoilou', 'Sci-Fi', 'PG-13', 'AM').
+movie('City of God', 'Alexandre Rodrigues', 8.6, 'Ste-Foy', 'Crime', 'R', 'PM').
+movie('Life Is Beautiful', 'Roberto Benigni', 8.6, 'Beauport', 'Comédie', 'PG-13', 'Soir').
+movie('Spirited Away', 'Rumi Hiiragi', 8.6, 'Limoilou', 'Animation', 'PG', 'AM').
+movie('The Usual Suspects', 'Kevin Spacey', 8.5, 'Ste-Foy', 'Crime', 'R', 'PM').
+movie('Léon: The Professional', 'Jean Reno', 8.5, 'Beauport', 'Crime', 'R', 'Soir').
+movie('The Lion King', 'Matthew Broderick', 8.5, 'Limoilou', 'Animation', 'G', 'AM').
+movie('The Pianist', 'Adrien Brody', 8.5, 'Ste-Foy', 'Biographie', 'R', 'PM').
+movie('Terminator 2: Judgment Day', 'Arnold Schwarzenegger', 8.5, 'Beauport', 'Action', 'R', 'Soir').
+movie('Gladiator', 'Russell Crowe', 8.5, 'Limoilou', 'Action', 'R', 'AM').
+movie('American History X', 'Edward Norton', 8.5, 'Ste-Foy', 'Drame', 'R', 'PM').
+movie('The Departed', 'Leonardo DiCaprio', 8.5, 'Beauport', 'Crime', 'R', 'Soir').
+movie('Whiplash', 'Miles Teller', 8.5, 'Limoilou', 'Drame', 'R', 'AM').
+movie('The Prestige', 'Hugh Jackman', 8.5, 'Ste-Foy', 'Drame', 'PG-13', 'PM').
+movie('The Dark Knight Rises', 'Christian Bale', 8.4, 'Beauport', 'Action', 'PG-13', 'Soir').
+movie('Avengers: Endgame', 'Robert Downey Jr.', 8.4, 'Limoilou', 'Action', 'PG-13', 'AM').
+movie('Django Unchained', 'Jamie Foxx', 8.4, 'Ste-Foy', 'Western', 'R', 'PM').
+movie('The Shining', 'Jack Nicholson', 8.4, 'Beauport', 'Horreur', 'R', 'Soir').
+movie('WALL·E', 'Ben Burtt', 8.4, 'Limoilou', 'Animation', 'G', 'AM').
+movie('Coco', 'Anthony Gonzalez', 8.4, 'Ste-Foy', 'Animation', 'PG', 'PM').
+movie('Braveheart', 'Mel Gibson', 8.4, 'Beauport', 'Biographie', 'R', 'Soir').
+movie('Joker', 'Joaquin Phoenix', 8.4, 'Limoilou', 'Drame', 'R', 'AM').
+movie('Avengers: Infinity War', 'Robert Downey Jr.', 8.4, 'Ste-Foy', 'Action', 'PG-13', 'PM').
+movie('Toy Story', 'Tom Hanks', 8.3, 'Beauport', 'Animation', 'G', 'Soir').
+movie('The Wolf of Wall Street', 'Leonardo DiCaprio', 8.2, 'Limoilou', 'Biographie', 'R', 'AM').
+movie('The Social Network', 'Jesse Eisenberg', 7.8, 'Ste-Foy', 'Biographie', 'PG-13', 'PM').
+movie('Cats', 'James Corden', 2.7, 'Limoilou', 'Musical', 'PG', 'AM').
+movie('The Room', 'Tommy Wiseau', 3.7, 'Ste-Foy', 'Drame', 'R', 'PM').
+movie('Disaster Movie', 'Matt Lanter', 1.9, 'Beauport', 'Comédie', 'PG-13', 'Soir').
+movie('Birdemic: Shock and Terror', 'Alan Bagh', 1.8, 'Ste-Foy', 'Horreur', 'R', 'AM').
+movie('Battlefield Earth', 'John Travolta', 2.5, 'Limoilou', 'Sci-Fi', 'PG-13', 'PM').
+movie('Manos: The Hands of Fate', 'Tom Neyman', 1.6, 'Beauport', 'Horreur', 'PG', 'Soir').
+movie('Movie 43', 'Hugh Jackman', 4.3, 'Ste-Foy', 'Comédie', 'R', 'AM').
+movie('Dragonball Evolution', 'Justin Chatwin', 2.6, 'Limoilou', 'Action', 'PG', 'PM').
+movie('Superbabies: Baby Geniuses 2', 'Jon Voight', 1.9, 'Beauport', 'Comédie', 'G', 'Soir').
+movie('Gigli', 'Ben Affleck', 2.6, 'Ste-Foy', 'Romance', 'R', 'AM').
+movie('The Love Guru', 'Mike Myers', 3.8, 'Limoilou', 'Comédie', 'PG-13', 'PM').
+movie('House of the Dead', 'Jonathan Cherry', 2.1, 'Beauport', 'Horreur', 'R', 'Soir').
+movie('Alone in the Dark', 'Christian Slater', 2.4, 'Ste-Foy', 'Horreur', 'R', 'AM').
+movie('Son of the Mask', 'Jamie Kennedy', 2.2, 'Limoilou', 'Comédie', 'PG', 'PM').
+movie('Kirk Cameron\'s Saving Christmas', 'Kirk Cameron', 1.3, 'Beauport', 'Famille', 'G', 'Soir').
+movie('Epic Movie', 'Kal Penn', 2.4, 'Ste-Foy', 'Comédie', 'PG-13', 'AM').
+movie('The Wicker Man', 'Nicolas Cage', 3.7, 'Limoilou', 'Thriller', 'PG-13', 'PM').
+movie('Battle: Los Angeles', 'Aaron Eckhart', 5.7, 'Beauport', 'Action', 'PG-13', 'Soir').
+movie('Pixels', 'Adam Sandler', 5.6, 'Ste-Foy', 'Comédie', 'PG-13', 'AM').
+movie('Transformers: Revenge of the Fallen', 'Shia LaBeouf', 6.0, 'Limoilou', 'Action', 'PG-13', 'PM').
+movie('The Happening', 'Mark Wahlberg', 5.0, 'Beauport', 'Thriller', 'R', 'Soir').
+movie('Fantastic Four', 'Miles Teller', 4.3, 'Ste-Foy', 'Sci-Fi', 'PG-13', 'AM').
+movie('Ghost Rider: Spirit of Vengeance', 'Nicolas Cage', 4.3, 'Limoilou', 'Action', 'PG-13', 'PM').
+movie('Suicide Squad', 'Will Smith', 5.9, 'Beauport', 'Action', 'PG-13', 'Soir').
+movie('The Twilight Saga: New Moon', 'Kristen Stewart', 4.7, 'Ste-Foy', 'Fantaisie', 'PG-13', 'AM').
+movie('Batman & Robin', 'George Clooney', 3.7, 'Limoilou', 'Action', 'PG-13', 'PM').
+movie('The Last Airbender', 'Noah Ringer', 4.1, 'Beauport', 'Aventure', 'PG', 'Soir').
+movie('Jaws: The Revenge', 'Michael Caine', 3.0, 'Ste-Foy', 'Thriller', 'PG-13', 'AM').
+movie('50 Shades of Grey', 'Dakota Johnson', 4.1, 'Limoilou', 'Romance', 'R', 'PM').
+movie('The Emoji Movie', 'T.J. Miller', 3.3, 'Beauport', 'Animation', 'PG', 'Soir').
 
 % Facts about movie theaters: movie_theater(Location, Price, Hours, Days).
-movie_theater('Ste-Foy', 12.50, ['12:30', '15:30', '20:30'], ['Monday', 'Wednesday', 'Saturday']).
+movie_theater('Ste-Foy', 12.50, ['AM', 'PM', 'Soir'], ['Lundi', 'Mardi', 'Mercredi', 'Samedi']).
 
-movie_theater('Beauport', 10.00, ['13:30', '17:30', '21:30'], ['Tuesday', 'Friday', 'Sunday']).
+movie_theater('Beauport', 10.00, ['AM', 'PM', 'Soir'], ['Mardi', 'Vendredi', 'Dimanche']).
 
-movie_theater('Limoilou', 9.75, ['11:30', '14:30', '19:30'], ['Wednesday', 'Thursday', 'Saturday']).
+movie_theater('Limoilou', 9.75, ['AM', 'PM', 'Soir'], ['Mardi', 'Mercredi', 'Jeudi', 'Samedi']).
 
 % Facts about snacks: snacks(Name, Price).
 snack('Chips', 1.50).
