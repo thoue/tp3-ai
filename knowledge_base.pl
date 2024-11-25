e88a8aa441d52399baab92d874f63d14f2723e5f
Pierre-Olivier Tremblay <pierre-olivier.tremblay.8@ulaval.ca>
2024-11-25 17:07:26 -0500

    added ratings

--- knowledge_base.pl
+++ knowledge_base.pl
@@ -1,85 +1,85 @@
-% Facts about movies: movie(MovieName, Actor, IMDBRating, MovieTheater, Genre).
-movie('The Shawshank Redemption', 'Tim Robbins', 9.3, 'Ste-Foy', 'Drama').
-movie('The Godfather', 'Marlon Brando', 9.2, 'Beauport', 'Crime').
-movie('The Dark Knight', 'Christian Bale', 9.0, 'Limoilou', 'Action').
-movie('The Godfather Part II', 'Al Pacino', 9.0, 'Ste-Foy', 'Crime').
-movie('12 Angry Men', 'Henry Fonda', 9.0, 'Beauport', 'Drama').
-movie('Schindler\'s List', 'Liam Neeson', 8.9, 'Limoilou', 'Biography').
-movie('The Lord of the Rings: The Return of the King', 'Elijah Wood', 8.9, 'Ste-Foy', 'Fantasy').
-movie('Pulp Fiction', 'John Travolta', 8.9, 'Beauport', 'Crime').
-movie('The Lord of the Rings: The Fellowship of the Ring', 'Elijah Wood', 8.8, 'Limoilou', 'Fantasy').
-movie('Forrest Gump', 'Tom Hanks', 8.8, 'Ste-Foy', 'Drama').
-movie('Inception', 'Leonardo DiCaprio', 8.8, 'Beauport', 'Sci-Fi').
-movie('The Lord of the Rings: The Two Towers', 'Elijah Wood', 8.8, 'Limoilou', 'Fantasy').
-movie('Fight Club', 'Brad Pitt', 8.8, 'Ste-Foy', 'Drama').
-movie('The Empire Strikes Back', 'Mark Hamill', 8.7, 'Beauport', 'Sci-Fi').
-movie('The Matrix', 'Keanu Reeves', 8.7, 'Limoilou', 'Sci-Fi').
-movie('Goodfellas', 'Robert De Niro', 8.7, 'Ste-Foy', 'Crime').
-movie('One Flew Over the Cuckoo\'s Nest', 'Jack Nicholson', 8.7, 'Beauport', 'Drama').
-movie('Se7en', 'Brad Pitt', 8.6, 'Limoilou', 'Crime').
-movie('The Silence of the Lambs', 'Jodie Foster', 8.6, 'Ste-Foy', 'Thriller').
-movie('It\'s a Wonderful Life', 'James Stewart', 8.6, 'Beauport', 'Drama').
-movie('Parasite', 'Song Kang-ho', 8.6, 'Limoilou', 'Thriller').
-movie('Saving Private Ryan', 'Tom Hanks', 8.6, 'Ste-Foy', 'War').
-movie('The Green Mile', 'Tom Hanks', 8.6, 'Beauport', 'Drama').
-movie('Interstellar', 'Matthew McConaughey', 8.6, 'Limoilou', 'Sci-Fi').
-movie('City of God', 'Alexandre Rodrigues', 8.6, 'Ste-Foy', 'Crime').
-movie('Life Is Beautiful', 'Roberto Benigni', 8.6, 'Beauport', 'Comedy').
-movie('Spirited Away', 'Rumi Hiiragi', 8.6, 'Limoilou', 'Animation').
-movie('The Usual Suspects', 'Kevin Spacey', 8.5, 'Ste-Foy', 'Crime').
-movie('Léon: The Professional', 'Jean Reno', 8.5, 'Beauport', 'Crime').
-movie('The Lion King', 'Matthew Broderick', 8.5, 'Limoilou', 'Animation').
-movie('The Pianist', 'Adrien Brody', 8.5, 'Ste-Foy', 'Biography').
-movie('Terminator 2: Judgment Day', 'Arnold Schwarzenegger', 8.5, 'Beauport', 'Action').
-movie('Gladiator', 'Russell Crowe', 8.5, 'Limoilou', 'Action').
-movie('American History X', 'Edward Norton', 8.5, 'Ste-Foy', 'Drama').
-movie('The Departed', 'Leonardo DiCaprio', 8.5, 'Beauport', 'Crime').
-movie('Whiplash', 'Miles Teller', 8.5, 'Limoilou', 'Drama').
-movie('The Prestige', 'Hugh Jackman', 8.5, 'Ste-Foy', 'Drama').
-movie('The Dark Knight Rises', 'Christian Bale', 8.4, 'Beauport', 'Action').
-movie('Avengers: Endgame', 'Robert Downey Jr.', 8.4, 'Limoilou', 'Action').
-movie('Django Unchained', 'Jamie Foxx', 8.4, 'Ste-Foy', 'Western').
-movie('The Shining', 'Jack Nicholson', 8.4, 'Beauport', 'Horror').
-movie('WALL·E', 'Ben Burtt', 8.4, 'Limoilou', 'Animation').
-movie('Coco', 'Anthony Gonzalez', 8.4, 'Ste-Foy', 'Animation').
-movie('Braveheart', 'Mel Gibson', 8.4, 'Beauport', 'Biography').
-movie('Joker', 'Joaquin Phoenix', 8.4, 'Limoilou', 'Drama').
-movie('Avengers: Infinity War', 'Robert Downey Jr.', 8.4, 'Ste-Foy', 'Action').
-movie('Toy Story', 'Tom Hanks', 8.3, 'Beauport', 'Animation').
-movie('The Wolf of Wall Street', 'Leonardo DiCaprio', 8.2, 'Limoilou', 'Biography').
-movie('The Social Network', 'Jesse Eisenberg', 7.8, 'Ste-Foy', 'Biography').
-movie('Cats', 'James Corden', 2.7, 'Limoilou', 'Musical').
-movie('The Room', 'Tommy Wiseau', 3.7, 'Ste-Foy', 'Drama').
-movie('Disaster Movie', 'Matt Lanter', 1.9, 'Beauport', 'Comedy').
-movie('Birdemic: Shock and Terror', 'Alan Bagh', 1.8, 'Ste-Foy', 'Horror').
-movie('Battlefield Earth', 'John Travolta', 2.5, 'Limoilou', 'Sci-Fi').
-movie('Manos: The Hands of Fate', 'Tom Neyman', 1.6, 'Beauport', 'Horror').
-movie('Movie 43', 'Hugh Jackman', 4.3, 'Ste-Foy', 'Comedy').
-movie('Dragonball Evolution', 'Justin Chatwin', 2.6, 'Limoilou', 'Action').
-movie('Superbabies: Baby Geniuses 2', 'Jon Voight', 1.9, 'Beauport', 'Comedy').
-movie('Gigli', 'Ben Affleck', 2.6, 'Ste-Foy', 'Romance').
-movie('The Love Guru', 'Mike Myers', 3.8, 'Limoilou', 'Comedy').
-movie('House of the Dead', 'Jonathan Cherry', 2.1, 'Beauport', 'Horror').
-movie('Alone in the Dark', 'Christian Slater', 2.4, 'Ste-Foy', 'Horror').
-movie('Son of the Mask', 'Jamie Kennedy', 2.2, 'Limoilou', 'Comedy').
-movie('Kirk Cameron\'s Saving Christmas', 'Kirk Cameron', 1.3, 'Beauport', 'Family').
-movie('Epic Movie', 'Kal Penn', 2.4, 'Ste-Foy', 'Comedy').
-movie('The Wicker Man', 'Nicolas Cage', 3.7, 'Limoilou', 'Thriller').
-movie('Battle: Los Angeles', 'Aaron Eckhart', 5.7, 'Beauport', 'Action').
-movie('Pixels', 'Adam Sandler', 5.6, 'Ste-Foy', 'Comedy').
-movie('Transformers: Revenge of the Fallen', 'Shia LaBeouf', 6.0, 'Limoilou', 'Action').
-movie('The Happening', 'Mark Wahlberg', 5.0, 'Beauport', 'Thriller').
-movie('Fantastic Four', 'Miles Teller', 4.3, 'Ste-Foy', 'Sci-Fi').
-movie('Ghost Rider: Spirit of Vengeance', 'Nicolas Cage', 4.3, 'Limoilou', 'Action').
-movie('Suicide Squad', 'Will Smith', 5.9, 'Beauport', 'Action').
-movie('The Twilight Saga: New Moon', 'Kristen Stewart', 4.7, 'Ste-Foy', 'Fantasy').
-movie('Batman & Robin', 'George Clooney', 3.7, 'Limoilou', 'Action').
-movie('The Last Airbender', 'Noah Ringer', 4.1, 'Beauport', 'Adventure').
-movie('Jaws: The Revenge', 'Michael Caine', 3.0, 'Ste-Foy', 'Thriller').
-movie('50 Shades of Grey', 'Dakota Johnson', 4.1, 'Limoilou', 'Romance').
-movie('The Emoji Movie', 'T.J. Miller', 3.3, 'Beauport', 'Animation').
+% Syntax: movie(Title, LeadActor, Rating, Location, Genre, RatingCategory).
+movie('The Shawshank Redemption', 'Tim Robbins', 9.3, 'Ste-Foy', 'Drama', 'R').
+movie('The Godfather', 'Marlon Brando', 9.2, 'Beauport', 'Crime', 'R').
+movie('The Dark Knight', 'Christian Bale', 9.0, 'Limoilou', 'Action', 'PG-13').
+movie('The Godfather Part II', 'Al Pacino', 9.0, 'Ste-Foy', 'Crime', 'R').
+movie('12 Angry Men', 'Henry Fonda', 9.0, 'Beauport', 'Drama', 'PG').
+movie('Schindler\'s List', 'Liam Neeson', 8.9, 'Limoilou', 'Biography', 'R').
+movie('The Lord of the Rings: The Return of the King', 'Elijah Wood', 8.9, 'Ste-Foy', 'Fantasy', 'PG-13').
+movie('Pulp Fiction', 'John Travolta', 8.9, 'Beauport', 'Crime', 'R').
+movie('The Lord of the Rings: The Fellowship of the Ring', 'Elijah Wood', 8.8, 'Limoilou', 'Fantasy', 'PG-13').
+movie('Forrest Gump', 'Tom Hanks', 8.8, 'Ste-Foy', 'Drama', 'PG-13').
+movie('Inception', 'Leonardo DiCaprio', 8.8, 'Beauport', 'Sci-Fi', 'PG-13').
+movie('The Lord of the Rings: The Two Towers', 'Elijah Wood', 8.8, 'Limoilou', 'Fantasy', 'PG-13').
+movie('Fight Club', 'Brad Pitt', 8.8, 'Ste-Foy', 'Drama', 'R').
+movie('The Empire Strikes Back', 'Mark Hamill', 8.7, 'Beauport', 'Sci-Fi', 'PG').
+movie('The Matrix', 'Keanu Reeves', 8.7, 'Limoilou', 'Sci-Fi', 'R').
+movie('Goodfellas', 'Robert De Niro', 8.7, 'Ste-Foy', 'Crime', 'R').
+movie('One Flew Over the Cuckoo\'s Nest', 'Jack Nicholson', 8.7, 'Beauport', 'Drama', 'R').
+movie('Se7en', 'Brad Pitt', 8.6, 'Limoilou', 'Crime', 'R').
+movie('The Silence of the Lambs', 'Jodie Foster', 8.6, 'Ste-Foy', 'Thriller', 'R').
+movie('It\'s a Wonderful Life', 'James Stewart', 8.6, 'Beauport', 'Drama', 'PG').
+movie('Parasite', 'Song Kang-ho', 8.6, 'Limoilou', 'Thriller', 'R').
+movie('Saving Private Ryan', 'Tom Hanks', 8.6, 'Ste-Foy', 'War', 'R').
+movie('The Green Mile', 'Tom Hanks', 8.6, 'Beauport', 'Drama', 'R').
+movie('Interstellar', 'Matthew McConaughey', 8.6, 'Limoilou', 'Sci-Fi', 'PG-13').
+movie('City of God', 'Alexandre Rodrigues', 8.6, 'Ste-Foy', 'Crime', 'R').
+movie('Life Is Beautiful', 'Roberto Benigni', 8.6, 'Beauport', 'Comedy', 'PG-13').
+movie('Spirited Away', 'Rumi Hiiragi', 8.6, 'Limoilou', 'Animation', 'PG').
+movie('The Usual Suspects', 'Kevin Spacey', 8.5, 'Ste-Foy', 'Crime', 'R').
+movie('Léon: The Professional', 'Jean Reno', 8.5, 'Beauport', 'Crime', 'R').
+movie('The Lion King', 'Matthew Broderick', 8.5, 'Limoilou', 'Animation', 'G').
+movie('The Pianist', 'Adrien Brody', 8.5, 'Ste-Foy', 'Biography', 'R').
+movie('Terminator 2: Judgment Day', 'Arnold Schwarzenegger', 8.5, 'Beauport', 'Action', 'R').
+movie('Gladiator', 'Russell Crowe', 8.5, 'Limoilou', 'Action', 'R').
+movie('American History X', 'Edward Norton', 8.5, 'Ste-Foy', 'Drama', 'R').
+movie('The Departed', 'Leonardo DiCaprio', 8.5, 'Beauport', 'Crime', 'R').
+movie('Whiplash', 'Miles Teller', 8.5, 'Limoilou', 'Drama', 'R').
+movie('The Prestige', 'Hugh Jackman', 8.5, 'Ste-Foy', 'Drama', 'PG-13').
+movie('The Dark Knight Rises', 'Christian Bale', 8.4, 'Beauport', 'Action', 'PG-13').
+movie('Avengers: Endgame', 'Robert Downey Jr.', 8.4, 'Limoilou', 'Action', 'PG-13').
+movie('Django Unchained', 'Jamie Foxx', 8.4, 'Ste-Foy', 'Western', 'R').
+movie('The Shining', 'Jack Nicholson', 8.4, 'Beauport', 'Horror', 'R').
+movie('WALL·E', 'Ben Burtt', 8.4, 'Limoilou', 'Animation', 'G').
+movie('Coco', 'Anthony Gonzalez', 8.4, 'Ste-Foy', 'Animation', 'PG').
+movie('Braveheart', 'Mel Gibson', 8.4, 'Beauport', 'Biography', 'R').
+movie('Joker', 'Joaquin Phoenix', 8.4, 'Limoilou', 'Drama', 'R').
+movie('Avengers: Infinity War', 'Robert Downey Jr.', 8.4, 'Ste-Foy', 'Action', 'PG-13').
+movie('Toy Story', 'Tom Hanks', 8.3, 'Beauport', 'Animation', 'G').
+movie('The Wolf of Wall Street', 'Leonardo DiCaprio', 8.2, 'Limoilou', 'Biography', 'R').
+movie('The Social Network', 'Jesse Eisenberg', 7.8, 'Ste-Foy', 'Biography', 'PG-13').
+movie('Cats', 'James Corden', 2.7, 'Limoilou', 'Musical', 'PG').
+movie('The Room', 'Tommy Wiseau', 3.7, 'Ste-Foy', 'Drama', 'R').
+movie('Disaster Movie', 'Matt Lanter', 1.9, 'Beauport', 'Comedy', 'PG-13').
+movie('Birdemic: Shock and Terror', 'Alan Bagh', 1.8, 'Ste-Foy', 'Horror', 'R').
+movie('Battlefield Earth', 'John Travolta', 2.5, 'Limoilou', 'Sci-Fi', 'PG-13').
+movie('Manos: The Hands of Fate', 'Tom Neyman', 1.6, 'Beauport', 'Horror', 'PG').
+movie('Movie 43', 'Hugh Jackman', 4.3, 'Ste-Foy', 'Comedy', 'R').
+movie('Dragonball Evolution', 'Justin Chatwin', 2.6, 'Limoilou', 'Action', 'PG').
+movie('Superbabies: Baby Geniuses 2', 'Jon Voight', 1.9, 'Beauport', 'Comedy', 'G').
+movie('Gigli', 'Ben Affleck', 2.6, 'Ste-Foy', 'Romance', 'R').
+movie('The Love Guru', 'Mike Myers', 3.8, 'Limoilou', 'Comedy', 'PG-13').
+movie('House of the Dead', 'Jonathan Cherry', 2.1, 'Beauport', 'Horror', 'R').
+movie('Alone in the Dark', 'Christian Slater', 2.4, 'Ste-Foy', 'Horror', 'R').
+movie('Son of the Mask', 'Jamie Kennedy', 2.2, 'Limoilou', 'Comedy', 'PG').
+movie('Kirk Cameron\'s Saving Christmas', 'Kirk Cameron', 1.3, 'Beauport', 'Family', 'G').
+movie('Epic Movie', 'Kal Penn', 2.4, 'Ste-Foy', 'Comedy', 'PG-13').
+movie('The Wicker Man', 'Nicolas Cage', 3.7, 'Limoilou', 'Thriller', 'PG-13').
+movie('Battle: Los Angeles', 'Aaron Eckhart', 5.7, 'Beauport', 'Action', 'PG-13').
+movie('Pixels', 'Adam Sandler', 5.6, 'Ste-Foy', 'Comedy', 'PG-13').
+movie('Transformers: Revenge of the Fallen', 'Shia LaBeouf', 6.0, 'Limoilou', 'Action', 'PG-13').
+movie('The Happening', 'Mark Wahlberg', 5.0, 'Beauport', 'Thriller', 'R').
+movie('Fantastic Four', 'Miles Teller', 4.3, 'Ste-Foy', 'Sci-Fi', 'PG-13').
+movie('Ghost Rider: Spirit of Vengeance', 'Nicolas Cage', 4.3, 'Limoilou', 'Action', 'PG-13').
+movie('Suicide Squad', 'Will Smith', 5.9, 'Beauport', 'Action', 'PG-13').
+movie('The Twilight Saga: New Moon', 'Kristen Stewart', 4.7, 'Ste-Foy', 'Fantasy', 'PG-13').
+movie('Batman & Robin', 'George Clooney', 3.7, 'Limoilou', 'Action', 'PG-13').
+movie('The Last Airbender', 'Noah Ringer', 4.1, 'Beauport', 'Adventure', 'PG').
+movie('Jaws: The Revenge', 'Michael Caine', 3.0, 'Ste-Foy', 'Thriller', 'PG-13').
+movie('50 Shades of Grey', 'Dakota Johnson', 4.1, 'Limoilou', 'Romance', 'R').
+movie('The Emoji Movie', 'T.J. Miller', 3.3, 'Beauport', 'Animation', 'PG').
 
-% Facts about movie theaters: movie_theater(Location, Price, Hours, Days, Movies).
+% Facts about movie theaters: movie_theater(Location, Price, Hours, Days).
 movie_theater('Ste-Foy', 12.50, ['12:00', '15:30', '20:00'], ['Monday', 'Wednesday', 'Saturday']).
 
 movie_theater('Beauport', 10.00, ['13:00', '17:00', '21:30'], ['Tuesday', 'Friday', 'Sunday']).
@@ -87,9 +87,9 @@ movie_theater('Beauport', 10.00, ['13:00', '17:00', '21:30'], ['Tuesday', 'Frida
 movie_theater('Limoilou', 9.75, ['11:00', '14:30', '19:00'], ['Wednesday', 'Thursday', 'Saturday']).
 
 % Facts about snacks: snacks(Name, Price).
-snacks('Chips', 1.50).
-snacks('Chocolat', 2.25).
-snacks('Popcorn', 5.00).
-snacks('Liqueur', 1.25).
-snacks('Bonbons', 3.00).
-snacks('Nachos', 6.50).
+snack('Chips', 1.50).
+snack('Chocolat', 2.25).
+snack('Popcorn', 5.00).
+snack('Liqueur', 1.25).
+snack('Bonbons', 3.00).
+snack('Nachos', 6.50).
